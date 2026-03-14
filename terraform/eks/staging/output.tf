22db4715a027851c6b2cc27d32a7aa15acad4950
k5sha <gositer2003@gmail.com>
2026-03-14 20:46:26 +0200

    fix: agro cd

--- terraform/eks/staging/output.tf
+++ terraform/eks/staging/output.tf
@@ -4,9 +4,16 @@ output "configure_kubectl" {
 }
 
 output "retail_app_url" {
-  description = "URL to access the retail store application"
-  value = try(
-    "http://${data.kubernetes_service.ui_service.status[0].load_balancer[0].ingress[0].hostname}",
-    "LoadBalancer provisioning - run: kubectl get svc -n ui ui"
+  description = "URL to access the retail store application (GitOps: ui-staging LB, else: ui LB)"
+  value = var.gitops_enabled ? (
+    try(
+      "http://${data.kubernetes_service.ui_staging_service[0].status[0].load_balancer[0].ingress[0].hostname}",
+      "LoadBalancer provisioning - run: kubectl get svc -n ui-staging"
+    )
+    ) : (
+    try(
+      "http://${data.kubernetes_service.ui_service[0].status[0].load_balancer[0].ingress[0].hostname}",
+      "LoadBalancer provisioning - run: kubectl get svc -n ui ui"
+    )
   )
 }
